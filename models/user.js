ba18dc8adef0129437d81db4c77ef984dceced49
sara <saraebrahimgeorge@gmail.com>
2025-06-05 00:20:05 -0300

    Adiciona o modelo User com validação de e-mail usando Validator

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please enter a valid email address",
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
